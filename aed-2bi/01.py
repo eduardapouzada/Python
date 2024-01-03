a1acbc5e324d38ecbf61a3a435ecafd3e85ee439
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-01-03 15:14:37 -0300

    exercicio 1

---
+++ aed-2bi/01.py
@@ -0,0 +1,10 @@
+""" 
+ Considere um programa em Python que simula um jogo de cartas com um
+baralho completo. O baralho é inicializado com 52 cartas, divididas em 4 naipes
+(Paus, Ouros, Copas e Espadas) e 13 valores (de 2 a 10, Valete, Dama, Rei e Ás). O
+programa deve utilizar listas para armazenar o baralho e sortear duas cartas para
+cada um de 6 jogadores. Mostre na tela cada uma das cartas sorteadas para cada
+jogador. Você não deve permitir que uma carta seja sorteada mais de uma vez.
+
+"""
+
