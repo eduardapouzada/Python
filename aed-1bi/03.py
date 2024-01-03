8b18b3942626da1f05d555d1ec13f885315bf397
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-01-03 14:42:47 -0300

    exercicio 3

---
+++ aed-1bi/03.py
@@ -0,0 +1,21 @@
+""" 
+Crie um programa em Python que solicite ao usuário um número inteiro
+positivo. O programa deve dividir esse número sucessivamente por 2 até que o
+resultado seja menor que 1. Em cada divisão, o programa deve exibir o resultado da
+divisão. Ao chegar a um resultado menor que 1, o programa deve exibir a mensagem
+"Chegou a zero!".
+Exemplo:
+Digite um número inteiro positivo: 8
+Resultado da divisão: 4.0
+Resultado da divisão: 2.0
+Resultado da divisão: 1.0
+Chegou a zero
+"""
+
+num = float(input('Digite um número para dividir: '))
+
+while num > 1:
+    num = num //2 
+    print(f'Resultado da divisão: {num}')
+    
+print('Chegou a zero!')    
